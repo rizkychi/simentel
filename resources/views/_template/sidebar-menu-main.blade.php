7f6d52d5248dfb06c3e901a2284bfd6a9af2a3a7
rizkychi <rizkynhae@gmail.com>
2023-03-29 14:19:29 +0700

    fix menu sidebar

--- resources/views/_template/sidebar-menu-main.blade.php
+++ resources/views/_template/sidebar-menu-main.blade.php
@@ -1,11 +1,26 @@
 @php
-    $name = request()->route()->getName();
+    $rute = request()->route()->getName();
+    $count = substr_count($rute, '.');
+    if ($count > 1) {
+        $name = request()->segment(1) . '.' . request()->segment(2);
+    } else {
+        $name = request()->segment(1);
+    }
+    $child = [];
+    foreach ((array) $list_menu as $idx => $menu) {
+        foreach ((array) $menu->child as $child) {
+            if (strpos($child->route, $name) !== false) {
+                $child[$idx] = 'menu-open';
+                break;
+            }
+        }
+    }
 @endphp
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        @foreach ($list_menu as $menu)
-            <li class="nav-item nav-parent">
-                <a href="{{ $menu->route ? route($menu->route) : '#' }}" class="nav-link {{ $name == $menu->route ? 'active':'' }}">
+        @foreach ($list_menu as $idx => $menu)
+            <li class="nav-item {{ $child[$idx] }}">
+                <a href="{{ $menu->route ? route($menu->route) : '#' }}" class="nav-link {{ strpos($menu->route,$name) !== false ? 'active':'' }}">
                     <i class="nav-icon {{ $menu->icon_type ?? 'fas' }} {{ $menu->icon ?? 'fa-circle' }}"></i>
                     <p>
                         {{ $menu->menu }}
@@ -18,7 +33,7 @@
                     <ul class="nav nav-treeview">
                         @foreach ($menu->child as $child)
                             <li class="nav-item">
-                                <a href="{{ $child->route ? route($child->route) : '#' }}" class="nav-link nav-child {{ $name == $child->route ? 'active':'' }}">
+                                <a href="{{ $child->route ? route($child->route) : '#' }}" class="nav-link nav-child {{ strpos($child->route, $name) !== false ? 'active':'' }}">
                                     <i class="{{ $child->icon_type ?? 'fas' }} {{ $child->icon ?? 'fa-circle' }} nav-icon"></i>
                                     <p>{{ $child->menu }}</p>
                                 </a>
@@ -30,15 +45,3 @@
         @endforeach
     </ul>
 </nav>
-
-@push('scripts')
-<script type="text/javascript">
-    $(function() {
-        $('.nav-child').each(function(i, obj){
-            if ($(this).hasClass('active')) {
-                $(this).closest('.nav-parent').addClass('menu-is-opening menu-open')
-            }
-        })
-    })
-</script>
-@endpush
